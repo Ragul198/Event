4ca58f33367835676575c0a0755ae483bdb36e90
Ragul198 <ragul198a@gmail.com>
2025-07-31 16:18:29 +0530

    commit-5

--- event/src/App.jsx
+++ event/src/App.jsx
@@ -5,15 +5,15 @@ import ProtectedRoute from "./components/ProtectedRoute";
 import AdminRoute from "./components/AdminRoute.jsx";
 
 import Home from "./Pages/Home.jsx";
-import EventDetails from "./pages/EventDetails.jsx";
+import EventDetails from "./Pages/EventDetails.jsx";
 import Login from "./Pages/Login.jsx";
-import AdminDashboard from "./pages/AdminDashboard.jsx";
-import MyEvents from "./pages/MyEvents.jsx";
-import RegisterForm from "./pages/RegisterForm.jsx";
-import EventRegistrations from "./pages/EventRegistrations.jsx";
-import Profile from "./pages/Profile.jsx";
-import AuthCallback from "./pages/AuthCallback.jsx";
-import StudentSetup from "./pages/StudentSetup.jsx";
+import AdminDashboard from "./Pages/AdminDashboard.jsx";
+import MyEvents from "./Pages/MyEvents.jsx";
+import RegisterForm from "./Pages/RegisterForm.jsx";
+import EventRegistrations from "./Pages/EventRegistrations.jsx";
+import Profile from "./Pages/Profile.jsx";
+import AuthCallback from "./Pages/AuthCallback.jsx";
+import StudentSetup from "./Pages/StudentSetup.jsx";
 
 const App = () => {
   return (
